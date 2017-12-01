ce1e9c02cb3eef311eccfdd41f3a695c5e986a7c
xiaofang <1026534229@qq.com>
2017-12-01 17:39:46 +0800

    post请求。以我们公司的服务为例实现的。

--- NetWork/NetWork/ViewController.m
+++ NetWork/NetWork/ViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [self GETData];
+    //[self GETData];
+    [self PostWork];
 }
 - (void)GETData{
     NSURL *url = [NSURL URLWithString:@"http://"];
@@ -37,6 +38,35 @@
 }
 - (void)PostWork {
     
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURL *url = [NSURL URLWithString:@"http://171.34.43.000:8000/login"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    NSDictionary *dic = @{
+                            @"biz":@{
+                                    @"username":@"xxff",
+                                    @"device_id" : @"d41d8cd98f00b204e9800998ecf8427e",
+                                    @"password" : @"xxxxx"
+
+                                    },
+                           
+                    };
+    NSData *paramData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:nil];
+    NSString *paramStr = [[NSString alloc] initWithData:paramData encoding:NSUTF8StringEncoding];
+    paramStr = [NSString stringWithFormat:@"xmas-json=%@", paramStr];
+    request.HTTPBody = [paramStr dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSURLSessionDataTask *sessionDataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error) {
+            NSLog(@"222Error: %@", error);
+        } else {
+            NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            NSLog(@"11%@", dict);
+        }
+    }];
+    [sessionDataTask resume];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
